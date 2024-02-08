34b5554940fb37cb06b53eeb9200b7de63ab7c6a
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-08 21:38:58 +0300

    Lecture2: Entity and EntityBase initializations.

---
+++ MVC_Blog/MVCBlog.Entity/Entities/Image.cs
@@ -0,0 +1,12 @@
+﻿using MVCBlog.Core.Entities;
+
+namespace MVCBlog.Entity.Entities
+{
+	public class Image : EntityBase, IEntityBase
+	{
+        public Guid Id { get; set; }
+		public string FileName { get; set; }
+		public string FileType { get; set; }
+		public ICollection<Article> Articles { get; set; }
+    }
+}

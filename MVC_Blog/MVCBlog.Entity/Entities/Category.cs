a934ec2799455be024b33e65fdd462c64f908c1e
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-08 22:16:43 +0300

    Lecture3: Database migration and packages.

--- MVC_Blog/MVCBlog.Entity/Entities/Category.cs
+++ MVC_Blog/MVCBlog.Entity/Entities/Category.cs
@@ -2,7 +2,7 @@
 
 namespace MVCBlog.Entity.Entities
 {
-	public class Category : EntityBase, IEntityBase
+	public class Category : EntityBase
 	{
 		public string Name { get; set; }
 		public ICollection<Article> Articles { get; set; }

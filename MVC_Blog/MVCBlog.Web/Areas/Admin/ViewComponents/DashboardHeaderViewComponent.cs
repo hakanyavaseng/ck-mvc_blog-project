22c1a908746100d5bef053a03b6098e495104e2b
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-13 12:17:09 +0300

    Lecture33: ViewComponent for dashboard header and role refactoring.

---
+++ MVC_Blog/MVCBlog.Web/Areas/Admin/ViewComponents/DashboardHeaderViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MVCBlog.Entity.DTOs.Users;
+using MVCBlog.Entity.Entities.Identity;
+
+namespace MVCBlog.Web.Areas.Admin.ViewComponents
+{
+    public class DashboardHeaderViewComponent : ViewComponent
+    {
+
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public DashboardHeaderViewComponent(UserManager<AppUser> userManager,IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var loggedInUser = await _userManager.GetUserAsync(HttpContext.User);
+            var map = _mapper.Map<UserDto>(loggedInUser);
+
+            var role = string.Join("",await _userManager.GetRolesAsync(loggedInUser));
+            map.Role = role;
+              
+
+            return View(map);
+        }
+
+    }
+}
---
+++ MVC_Blog/MVCBlog.Web/Consts/RoleConsts.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCBlog.Web.Consts
+{
+    public static class RoleConsts
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+    }
+}
